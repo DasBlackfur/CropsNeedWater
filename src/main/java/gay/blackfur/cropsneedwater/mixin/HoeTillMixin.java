5ed875dbd75afbbdbbcac10146b542e49cc493cd
DasBlackfur <64478051+DasBlackfur@users.noreply.github.com>
2024-11-17 21:30:00 +0100

    Farmland is now waterlogged if tilled underwater

--- src/main/java/gay/blackfur/cropsneedwater/mixin/HoeTillMixin.java
+++ src/main/java/gay/blackfur/cropsneedwater/mixin/HoeTillMixin.java
@@ -1,7 +1,12 @@
 package gay.blackfur.cropsneedwater.mixin;
 
+import com.llamalad7.mixinextras.sugar.Local;
+import gay.blackfur.cropsneedwater.mixin.configured.WaterloggedMarker;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.neoforged.neoforge.common.extensions.IBlockExtension;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,5 +17,14 @@ public interface HoeTillMixin {
     @Redirect(method = "getToolModifiedState", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockState;isAir()Z"))
     private boolean allowWaterAboveTilling(BlockState instance) {
         return instance.isAir() || instance.is(Blocks.WATER);
+    }
+
+    @Redirect(method = "getToolModifiedState", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/Block;defaultBlockState()Lnet/minecraft/world/level/block/state/BlockState;"))
+    private BlockState waterloggedIfUnderwater(Block instance, @Local(argsOnly = true) UseOnContext context) {
+        var state = instance.defaultBlockState();
+        if (instance instanceof WaterloggedMarker && context.getLevel().getBlockState(context.getClickedPos().above()).is(Blocks.WATER)) {
+            state = state.setValue(BlockStateProperties.WATERLOGGED, true);
+        }
+        return state;
     }
 }
