296497013a92736ef0ee0d9ec690b1bae6946387
DasBlackfur <64478051+DasBlackfur@users.noreply.github.com>
2025-03-18 20:00:26 +0100

    Fixed compatibility with new release of Immersive Engineering

--- src/main/java/gay/blackfur/cropsneedwater/mixin/configured/immersive_engineering/CanGrowTopMixin.java
+++ src/main/java/gay/blackfur/cropsneedwater/mixin/configured/immersive_engineering/CanGrowTopMixin.java
@@ -1,9 +1,11 @@
 package gay.blackfur.cropsneedwater.mixin.configured.immersive_engineering;
 
+import blusunrize.immersiveengineering.common.blocks.plant.HempBlock;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.level.block.state.properties.DoubleBlockHalf;
+import net.minecraft.world.level.block.state.properties.EnumProperty;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
@@ -17,10 +19,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class CanGrowTopMixin {
     @Final
     @Shadow
-    public static BooleanProperty TOP;
+    public static EnumProperty<DoubleBlockHalf> HALF;
     @Inject(method = "canGrowTop", at = @At("HEAD"), cancellable = true)
     public void addWaterCheck(LevelReader world, BlockPos pos, BlockState state, CallbackInfoReturnable<Boolean> cir) {
-        if ((!state.getValue(TOP)) && world.isWaterAt(pos.above())) {
+        if (state.getValue(HALF) != DoubleBlockHalf.UPPER && world.isWaterAt(pos.above())) {
             cir.setReturnValue(true);
         } else {
             cir.setReturnValue(false);
